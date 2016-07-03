c95bdad890410e74fafe6d8e0bd544bec321be8f
Victor <victor.hugo.origins@gmail.com>
2016-07-03 16:18:00 -0300

    Interdace ShortCourseBusiness created

--- src/main/java/io/github/herocode/ecmat/interfaces/ShortCourseBusiness.java
+++ src/main/java/io/github/herocode/ecmat/interfaces/ShortCourseBusiness.java
@@ -3,80 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package io.github.herocode.ecmat.model;
+package io.github.herocode.ecmat.interfaces;
 
 import io.github.herocode.ecmat.entity.Participant;
 import io.github.herocode.ecmat.entity.ShortCourse;
-import io.github.herocode.ecmat.interfaces.Dao;
-import io.github.herocode.ecmat.interfaces.ShortCourseDaoFunctions;
-import java.sql.ResultSet;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
  * @author Victor Hugo <victor.hugo.origins@gmail.com>
  */
-public class ShortCourseBusiness implements ShortCourseDaoFunctions, Dao<ShortCourse, Integer>{
+public interface ShortCourseBusiness {
 
-    @Override
-    public List<Participant> getShortCourseParticipants(ShortCourse shortCourse) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    boolean addParticipant(ShortCourse shortCourse, Participant participant);
 
-    @Override
-    public int getCurrentEnrollment(ShortCourse shortCourse) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    boolean delete(ShortCourse object);
 
-    @Override
-    public boolean removeParticipant(ShortCourse shortCourse, Participant participant) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    int getCurrentEnrollment(ShortCourse shortCourse);
 
-    @Override
-    public boolean addParticipant(ShortCourse shortCourse, Participant participant) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    List<Participant> getShortCourseParticipants(ShortCourse shortCourse);
 
-    @Override
-    public boolean save(ShortCourse object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    List<ShortCourse> listAll();
 
-    @Override
-    public boolean delete(ShortCourse object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    boolean removeParticipant(ShortCourse shortCourse, Participant participant);
 
-    @Override
-    public boolean update(ShortCourse object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    boolean save(ShortCourse object);
 
-    @Override
-    public ShortCourse searchById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    ShortCourse searchById(Integer id);
 
-    @Override
-    public List<ShortCourse> searchByAttributes(Map<String, String> map) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<ShortCourse> listAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ShortCourse fillObject(ResultSet rs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String getTableName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    boolean update(ShortCourse object);
     
 }
