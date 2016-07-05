022f6b8de4b8ca6febcbe213c6794c4570263854
Victor <victor.hugo.origins@gmail.com>
2016-07-05 15:42:38 -0300

    New error messages added

--- src/main/java/io/github/herocode/ecmat/enums/ErrorMessage.java
+++ src/main/java/io/github/herocode/ecmat/enums/ErrorMessage.java
@@ -10,22 +10,27 @@ package io.github.herocode.ecmat.enums;
  * @author Victor Hugo <victor.hugo.origins@gmail.com>
  */
 public enum ErrorMessage {
- 
-    EMPTY_NAME("Campo nome vazio, informe seu nome."), 
-    EMPTY_EMAIL("Campo e-mail vazio, informe um e-mail válido."), 
+
+    EMPTY_NAME("Campo nome vazio, informe seu nome."),
+    EMPTY_EMAIL("Campo e-mail vazio, informe um e-mail válido."),
     EMPTY_CPF("Campo CPF vazio, informe um CPF válido."),
     
-    INVALID_CPF("O CPF informado é inválido"),
-    INVALID_EMAIL("O e-mail informado é inválido");
+    INVALID_CPF("O CPF informado é inválido."),
+    INVALID_EMAIL("O e-mail informado é inválido."),
     
+    FILLED_SHORT_COURSE("O mini curso está com todas as vagas preenchidas."),
+    PARTICIPANT_ALREADY_REGISTERED("O participante já está matriculado neste mini curso."),
+    PARTICIPANT_IS_ENROLLED_IN_ANOTHER_SHORT_COURSE("O participante ja está matriculado para"
+            + " um minicurso no mesmo horário");
+
     String errorMessage;
-    
-    ErrorMessage(String message){
+
+    ErrorMessage(String message) {
         errorMessage = message;
     }
-    
-    public String getErrorMessage(){
+
+    public String getErrorMessage() {
         return errorMessage;
     }
-    
+
 }
