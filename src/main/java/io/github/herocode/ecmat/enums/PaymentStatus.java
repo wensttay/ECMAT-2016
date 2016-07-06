790410c3f77e4eb8521c8291009f5ce48584f551
Victor <victor.hugo.origins@gmail.com>
2016-07-06 17:31:01 -0300

    PaymentStatus enum created, this enum is based in PagSeguro status system

---
+++ src/main/java/io/github/herocode/ecmat/enums/PaymentStatus.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.herocode.ecmat.enums;
+
+/**
+ *
+ * @author Victor Hugo <victor.hugo.origins@gmail.com>
+ */
+public enum PaymentStatus {
+
+    AWAITING_PAYMENT(1),
+    IN_REVIEW(2),
+    COMPLETE(3),
+    AVAILABLE(4),
+    IN_DISPUTE(5),
+    RETURNED(6),
+    CANCELLED(7);
+
+    int code;
+
+    PaymentStatus(int paymentCode) {
+        code = paymentCode;
+    }
+
+    public int getPaymentStatusCode() {
+        return code;
+    }
+
+}
